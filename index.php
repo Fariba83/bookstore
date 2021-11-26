9a0044c78b0c1b8d09a4cfd3702d3b8f9bd52174
fariba83 <fmahboubrezaei4316@conestogac.on.ca>
2021-11-26 08:41:03 -0500

    init

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <!-- bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+     integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
+   </script>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img class="logo" src="images/logo1.png" width="120px" height="auto" alt=""></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="signin.php">SIGN IN</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="signup.php">SIGN UP</a>
+              </li>
+                        
+              <li class="nav-item">
+                <a class="nav-link" href="ebook.php">DOWNLOAD E-BOOKS</a>
+              </li>
+            </ul>
+  
+          </div>
+        </div>
+      </nav>
+    <div class="wrapper">
+     <div class="home-image">
+         <img src="images/hero.png" alt="">
+     </div>
+     <div class="home-content">
+         <h1>E-BOOK ACADEMY</h1>
+         <a href="signin.php">DOWNLOAD HERE</a>
+
+     </div>
+
+  
+
+
+
+
+</div>
+    
+</body>
+</html>
