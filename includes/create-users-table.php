c5e42b52c1777a8481c93bb504656690663975ef
Fariba83 <84359750+Fariba83@users.noreply.github.com>
2021-12-14 19:32:56 -0500

    tables query added

---
+++ includes/create-users-table.php
@@ -0,0 +1,10 @@
+<?php
+CREATE TABLE `users` (
+    `user_id` int(11) NOT NULL,
+    `userName` varchar(100) NOT NULL,
+    `Email` varchar(40) NOT NULL,
+    `password1` varchar(40) NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  
+  --
+  ?>
